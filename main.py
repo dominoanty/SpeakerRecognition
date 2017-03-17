3cbae9722d1a18758dec462d3b710d5ff1f2d31f
Ranganath Pai <ranganath.pai@gmail.com>
2017-03-17 10:30:26 +0530

    Restructured Code. Loading and saving models

--- main.py
+++ main.py
@@ -1,104 +1,143 @@
 import python_speech_features as psf
-from scipy.io import wavfile
-
-NO_SPEAKERS = 10
-
-
-#Load in data from .wav files in data/
-#Extract mfcc (first 13 coefficients) from each audio sample
-spk  = [wavfile.read('data/english' + str(i) + '.wav') for i in range(1, NO_SPEAKERS + 1)]
-spk_mfcc = [psf.mfcc(spk[i][1], spk[i][0])  for i in range(0, NO_SPEAKERS)]
-
-
-#Cepstral Mean Subtraction (Feature Normalization step)
-from functools import reduce
-for i, speaker_mfcc in enumerate(spk_mfcc):
-    average = reduce(lambda acc, ele: acc + ele, speaker_mfcc)
-    average = list(map(lambda x: x/len(speaker_mfcc), average))
-    for j, feature_vector in enumerate(speaker_mfcc):
-        for k, feature in enumerate(feature_vector):
-            spk_mfcc[i][j][k] -= average[k]
-
-
-#Create GMM UBM Models for each speaker and prepare data for training
 from sklearn.mixture import GaussianMixture
+from sklearn.externals import joblib
+from scipy.io import wavfile
+from functools import reduce
 
-GMM = []
-UBM = []
+MODEL_SPEAKERS = 50
+TOTAL_SPEAKERS = 50
 
-#  Create a GMM and UBM model for each speaker. The GMM is modelled after the speaker and UBM for each speaker
-#  is modelled after all the other speakers. Likelihood Ratio test is used to verify speaker
-def setGMMUBM(no_components):
-    global GMM, UBM
-    GMM = []
-    UBM = []
-    for i in range(NO_SPEAKERS):
-        GMM.append(GaussianMixture(n_components= no_components, covariance_type= 'diag'))
-        UBM.append(GaussianMixture(n_components= no_components, covariance_type= 'diag'))
+class SpeakerRecognition:
 
+    #  Create a GMM and UBM model for each speaker. The GMM is modelled after the speaker and UBM for each speaker
+    #  is modelled after all the other speakers. Likelihood Ratio test is used to verify speaker
+    def setGMMUBM(self, no_components):
+        for i in range(MODEL_SPEAKERS):
+            self.GMM.append(GaussianMixture(n_components= no_components, covariance_type= 'diag'))
+            self.UBM.append(GaussianMixture(n_components= no_components, covariance_type= 'diag'))
 
-# Partition data into training and testing
-total_mfcc = []
-speaker_label = []
-spk_train_size = []
-spk_start = []
-spk_end = []
+    # Load in data from .wav files in data/
+    # Extract mfcc (first 13 coefficients) from each audio sample
+    def load_data(self):
+        self.spk  = [wavfile.read('data/english' + str(i) + '.wav') for i in range(1, TOTAL_SPEAKERS + 1)]
+        self.spk_mfcc = [psf.mfcc(self.spk[i][1], self.spk[i][0])  for i in range(0, TOTAL_SPEAKERS)]
 
-for i in range(NO_SPEAKERS):
-    spk_train_size.append(int(0.9 * len(spk_mfcc[i])))
-    spk_start.append(len(total_mfcc))
-    print(i)
-    for mfcc in spk_mfcc[i][0:spk_train_size[i], :]:
-        total_mfcc.append(mfcc)
-        speaker_label.append(i)
-    spk_end.append(len(total_mfcc))
-
-
-# Fit the GMM UBM models with training data
-def fit_model():
-    for i in range(NO_SPEAKERS):
-        print("Fit start for {}".format(i))
-        GMM[i].fit(spk_mfcc[i])
-        UBM[i].fit(total_mfcc[:spk_start[i]] + total_mfcc[spk_end[i]:])
-        print("Fit end for {}".format(i))
+        for i in range(TOTAL_SPEAKERS):
+            self.spk_train_size.append(int(0.9 * len(self.spk_mfcc[i])))
+            self.spk_start.append(len(self.total_mfcc))
+            print(i)
+            for mfcc in self.spk_mfcc[i][0:self.spk_train_size[i], :]:
+                self.total_mfcc.append(mfcc)
+                self.speaker_label.append(i)
+            self.spk_end.append(len(self.total_mfcc))
 
 
-# Predict the output for each model for each speaker and produce confusion matrix
-def execute():
-    fit_model()
-    avg_accuracy = 0
 
-    confusion = [[ 0 for y in range(NO_SPEAKERS) ] for x in range(NO_SPEAKERS)]
+    # Gaussian Mixture Model is made of a number of Gaussian distribution components.
+    #  To model data, a suitable number o gaussian components have to be selected.
+    # There is no method for finding this. It is done by trial and error. This runs
+    # the program for different values of component and records accuracy for each one
+    def find_best_params(self):
+        best_no_components = 1
+        maxacc = 0
+        for i in range(1, 256):
+            self.setGMMUBM(i)
+            acc, _ = self.execute()
+            print("Accuracy for n = {} is {}".format(i, acc))
+            if acc > maxacc:
+                maxacc = acc
+                best_no_components = i
+        return best_no_components
 
-    for i in range(NO_SPEAKERS):
-        for j in range(NO_SPEAKERS):
-            x = GMM[j].score_samples(spk_mfcc[i][spk_train_size[i] + 2 : ]) - UBM[j].score_samples(spk_mfcc[i][spk_train_size[i] + 2 : ])
-            for score in x :
-                if score > 0:
-                    confusion[i][j] += 1
+    # Fit the GMM UBM models with training data
+    def fit_model(self):
+        for i in range(MODEL_SPEAKERS):
+            print("Fit start for {}".format(i))
+            self.GMM[i].fit(self.spk_mfcc[i])
+            self.UBM[i].fit(self.total_mfcc[:self.spk_start[i]] + self.total_mfcc[self.spk_end[i]:])
+            print("Fit end for {}".format(i))
+            joblib.dump(self.UBM[i], 'dumps/ubm' + str(i) + '.pkl')
+            joblib.dump(self.GMM[i], 'dumps/gmm' + str(i) + '.pkl')
 
-    confusion_diag = [confusion[i][i] for i in range(NO_SPEAKERS)]
+    def model(self, no_components = 244):
+        self.setGMMUBM(no_components)
+        self.fit_model()
 
-    diag_sum = 0
-    for item in confusion_diag:
-        diag_sum += item
+    # Predict the output for each model for each speaker and produce confusion matrix
+    def load_model(self):
+        for i in range(0, MODEL_SPEAKERS):
+            self.GMM.append(joblib.load('dumps/gmm' + str(i) + '.pkl'))
+            self.UBM.append(joblib.load('dumps/ubm' + str(i) + '.pkl'))
 
-    remain_sum = 0
-    for i in range(NO_SPEAKERS):
-        for j in range(NO_SPEAKERS):
-            if i != j:
-                remain_sum += confusion[i][j]
+    def predict(self):
+        avg_accuracy = 0
 
-    spk_accuracy = 0
-    for i in range(NO_SPEAKERS):
-        best_guess, _ = max(enumerate(confusion[i]), key=lambda p: p[1])
-        print("For speaker {}, best guess is {}".format(i, best_guess))
-        if i == best_guess:
-            spk_accuracy += 1
-    spk_accuracy /= NO_SPEAKERS
+        confusion = [[ 0 for y in range(MODEL_SPEAKERS) ] for x in range(TOTAL_SPEAKERS)]
 
-    avg_accuracy = diag_sum/(remain_sum+diag_sum)
-    return confusion, avg_accuracy, spk_accuracy
+        for i in range(TOTAL_SPEAKERS):
+            for j in range(MODEL_SPEAKERS):
+                x = self.GMM[j].score_samples(self.spk_mfcc[i][self.spk_train_size[i] + 2 : ]) - self.UBM[j].score_samples(self.spk_mfcc[i][self.spk_train_size[i] + 2 : ])
+                for score in x :
+                    if score > 0:
+                        confusion[i][j] += 1
+
+        confusion_diag = [confusion[i][i] for i in range(MODEL_SPEAKERS)]
+
+        diag_sum = 0
+        for item in confusion_diag:
+            diag_sum += item
+
+        remain_sum = 0
+        for i in range(MODEL_SPEAKERS):
+            for j in range(MODEL_SPEAKERS):
+                if i != j:
+                    remain_sum += confusion[i][j]
+
+        spk_accuracy = 0
+        for i in range(MODEL_SPEAKERS):
+            best_guess, _ = max(enumerate(confusion[i]), key=lambda p: p[1])
+            print("For speaker {}, best guess is {}".format(i, best_guess))
+            if i == best_guess:
+                spk_accuracy += 1
+        spk_accuracy /= MODEL_SPEAKERS
+
+        avg_accuracy = diag_sum/(remain_sum+diag_sum)
+        return confusion, avg_accuracy, spk_accuracy
+
+    def __init__(self):
+        self.test_spk = []
+        self.test_mfcc = []
+
+        # Speaker data and corresponding mfcc
+        self.spk = []
+        self.spk_mfcc = []
+
+        # Holds all the training mfccs of all speakers and
+        # speaker_label is the speaker label for the corresponding mfcc
+
+        self.total_mfcc = []
+        self.speaker_label = []
+        self.spk_train_size = []  # Index upto which is training data for that speaker.
+
+        # Since the length of all the audio files are different, spk_start and spk_end hold
+
+        self.spk_start = []
+        self.spk_end = []
+
+        self.GMM = []
+        self.UBM = []
+        self.load_data()
+        self.cepstral_mean_subtraction()
+
+    # Cepstral Mean Subtraction (Feature Normalization step)
+    def cepstral_mean_subtraction(self):
+        for i, speaker_mfcc in enumerate(self.spk_mfcc):
+            average = reduce(lambda acc, ele: acc + ele, speaker_mfcc)
+            average = list(map(lambda x: x/len(speaker_mfcc), average))
+            for j, feature_vector in enumerate(speaker_mfcc):
+                for k, feature in enumerate(feature_vector):
+                    self.spk_mfcc[i][j][k] -= average[k]
+
 
 
 #TBD : Ten fold validation
@@ -119,26 +158,16 @@ def ten_fold():
     print("Average accuracy  " + str(100 * average_accuracy))
     return average_accuracy
 
-# Gaussian Mixture Model is made of a number of Gaussian distribution components. To model data, a suitable number of
-# gaussian components have to be selected. There is no method for finding this. It is done by trial and error. This runs
-# the program for different values of component and records accuracy for each one
-def find_best_params():
-    best_no_components = 1
-    maxacc = 0
-    for i in range(1, 256):
-        setGMMUBM(i)
-        acc, _ = execute()
-        print("Accuracy for n = {} is {}".format(i, acc))
-        if acc > maxacc:
-            maxacc = acc
-            best_no_components = i
-    return best_no_components
+
 
 # Final result is a confusion matrix which represents the accuracy of the fit of the model
 import numpy as np
 if __name__ == '__main__':
-    setGMMUBM(244)
-    confusion, mfcc_accuracy, spk_accuracy = execute()
+
+    SR = SpeakerRecognition()
+    SR.load_model()
+    confusion, mfcc_accuracy, spk_accuracy = SR.predict()
+
     print("Confusion Matrix")
     print(np.matrix(confusion))
     print("Accuracy in predicting speakers : {}".format(spk_accuracy))
